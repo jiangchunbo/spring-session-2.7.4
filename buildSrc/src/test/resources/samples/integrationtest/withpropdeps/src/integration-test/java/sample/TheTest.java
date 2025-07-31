84fc3fb521f65bf9a58cf17118040e873ef3f1c7
jiangchunbo <jiangchunbo@tq-edu.com>
2025-07-31 23:49:43 +0800

    :bulb: 学习

---
+++ buildSrc/src/test/resources/samples/integrationtest/withpropdeps/src/integration-test/java/sample/TheTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+import javax.servlet.http.HttpServletRequest;
+
+public class TheTest {
+	@Test
+	public void compilesAndRuns() {
+		HttpServletRequest request = null;
+	}
+}
